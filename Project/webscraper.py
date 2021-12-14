73cbc6e3984453b1d7c31d4d24f15d4a41d1c991
darlyn <darlynnguyen6697@gmail.com>
2021-12-14 00:44:30 -0500

    :D

--- Project/webscraper.py
+++ Project/webscraper.py
@@ -244,7 +244,7 @@ if __name__ == '__main__':
     python_ta.check_all(config={
         'extra-imports': ['bs4.element', 'pandas', 'requests',
                           'bs4', 'pytrends.request'],  # the names (strs) of imported modules
-        'allowed-io': [],     # the names (strs) of functions that call print/open/input
+        'allowed-io': [],  # the names (strs) of functions that call print/open/input
         'max-line-length': 100,
         'disable': ['R1705', 'C0200', 'E9997']
     })
